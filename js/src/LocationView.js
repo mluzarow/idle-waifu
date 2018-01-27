24a4b92b2d4f81203cee7cd41cb8bbd1cc1a9f91
Mackenzie <ma.luzarow@gmail.com>
2018-01-27 11:39:48 -0600

    Added some sort of location prototype idea.

---
+++ js/src/LocationView.js
@@ -0,0 +1,14 @@
+class LocationView {
+	public construct () {
+		
+	}
+	
+	public generate () {
+		if (this.generatedHTML.length > 0) {
+			return (this.generatedHTML);
+		} else {
+			// Build out the view
+			
+		}
+	}
+}
---
+++ js/src/WaifuCore.js
@@ -0,0 +1,6 @@
+class WaifuCore {
+	static initialize () {
+		this.viewWindow = document.getElementsByTagName ("body")[0];
+		this.locationCache = {};
+	}
+}
