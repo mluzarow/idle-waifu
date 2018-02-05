0ec9ed17a30fac1fa60f3967c65cfad310e4eaab
Mackenzie <ma.luzarow@gmail.com>
2018-02-04 20:24:44 -0600

    fixed line spliting.

--- js/src/WaifuCore.js
+++ js/src/WaifuCore.js
@@ -20,18 +20,22 @@ class WaifuCore {
 		
 		// Text pre-processing
 		this.renderText = [];
+		this.context.font = "16px Arial";
+		
 		var test_text = "\"On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains.\"";
 		var split_text = test_text.split (' ');
 		var testLine = "";
 		
 		for (var i = 0; i < split_text.length; i++) {
-			testLine += " " + split_text[i];
-			var lineLength = this.context.measureText (testLine).width;
-			console.log ("Linelength: " + lineLength + " text: " + testLine);
+			var currentLine = testLine + " " + split_text[i];
+			var lineLength = this.context.measureText (currentLine).width;
+			console.log ("Linelength: " + lineLength + " text: " + currentLine);
 			
-			if (lineLength >= 720 || i === (split_text.length - 1)) {
+			if (lineLength >= 1150 || i === (split_text.length - 1)) {
 				this.renderText.push (testLine);
-				testLine = "";
+				testLine = split_text[i];
+			} else {
+				testLine = currentLine;
 			}
 		}
 		
