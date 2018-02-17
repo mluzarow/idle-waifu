59c9bf1819a02d05c1fbf7b265ea41439c7a0f93
Mackenzie <ma.luzarow@gmail.com>
2018-02-17 15:26:51 -0600

    Fixed auto-typing + added some comments.

--- js/src/WaifuCore.js
+++ js/src/WaifuCore.js
@@ -1,45 +1,73 @@
+/**
+ * Core process for the game. Initializes game control variables and maintains
+ * core game loops.
+ */
 class WaifuCore {
-	initialize () {
+	constructor () {
+		/**
+		 * @var {int} CANVAS_WIDTH  constant width of the canvas window
+		 * @var {int} CANVAS_HEIGHT constant height of the canvas window
+		 */
 		this.CANVAS_WIDTH = 1280;
 		this.CANVAS_HEIGHT = 640;
 		
+		/**
+		 * @var {Node}    canvas  canvas element
+		 * @var {Context} context 2D context for selected canvas
+		 */
 		this.canvas = document.getElementById ("canvas");
 		this.context = this.canvas.getContext ("2d");
 		
+		/**
+		 * @type {int} FPS cap for rendering. This also controls frequency at
+		 *             which the rendering method is called.
+		 */
 		this.fpsMax = 50;
 		
-		this.x = 0;
-		this.y = 0;
-		
+		/**
+		 * @var {int}  fpsDelta      number of frames since last update () was
+		 *                           called
+		 * @var {Date} lastIteration date stamp of last call to update ()
+		 */
 		this.fpsDelta = 0;
 		this.lastIteration = Date.now ();
+	}
+	
+	/**
+	 * Initialization process for all core game variables and intervals.
+	 */
+	initialize () {
 		this.testImg = new Image ();
 		this.testImg.src = "img/test-bg.jpg";
 		this.textboxImage = new Image ();
 		this.textboxImage.src = "img/textbox.png";
 		
 		// Text pre-processing
-		this.renderText = [];
+		this.renderText = [""];
 		this.context.font = "16px Arial";
 		
 		var test_text = "\"On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains.\"";
-		var split_text = test_text.split (' ');
-		var testLine = "";
 		
-		for (var i = 0; i < split_text.length; i++) {
-			var currentLine = testLine + " " + split_text[i];
-			var lineLength = this.context.measureText (currentLine).width;
-			console.log ("Linelength: " + lineLength + " text: " + currentLine);
-			
-			if (lineLength >= 1150 || i === (split_text.length - 1)) {
-				this.renderText.push (testLine);
-				testLine = split_text[i];
-			} else {
-				testLine = currentLine;
-			}
-		}
+		var testChunk = new DialogChunk (test_text, 1150, this.context);
+		this.processedText = testChunk.processedText;
+		this.printInterval = setInterval (this.printText.bind(this), 3000 / this.fpsMax);
 		
 		setInterval (this.update.bind(this), 1000 / this.fpsMax);
+	}
+	
+	printText () {
+		this.renderText[this.renderText.length - 1] += this.processedText[0].substring (0, 1);
+		this.processedText[0] = this.processedText[0].substring (1, this.processedText[0].length);
+		
+		if (this.processedText[0].length < 1) {
+			this.processedText.shift ();
+			
+			if (this.processedText.length < 1) {
+				clearInterval (this.printInterval);
+			} else {
+				this.renderText.push ("");
+			}
+		}
 	}
 	
 	update () {
@@ -47,8 +75,6 @@ class WaifuCore {
 		var localDelta = currentIteration - this.lastIteration;
 		this.fpsDelta += (localDelta - this.fpsDelta) / 20;
 		this.lastIteration = currentIteration;
-		this.x++;
-		this.y++;
 		this.draw ();
 	}
 	
